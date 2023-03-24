5e3f2b35308b375692e82364f14fedc380fbc93c
juanmadev5 <juanma261005@gmail.com>
2023-03-24 19:11:36 -0300

    new icon added!

--- app/src/main/java/com/jmdev/app/flashlight/MainActivity.kt
+++ app/src/main/java/com/jmdev/app/flashlight/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -17,7 +18,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,14 +48,13 @@ fun MainScreen() {
 
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = Color.Black
+        color = MaterialTheme.colorScheme.surface
     ) {
         Text(
             text = "Linterna " + lightMsg.value,
             Modifier.padding(32.dp, top = 60.dp),
             fontSize = 30.sp,
-            fontWeight = FontWeight.Light,
-            color = Color.White
+            fontWeight = FontWeight.Light
         )
         Box(contentAlignment = Alignment.Center) {
             Switch(
